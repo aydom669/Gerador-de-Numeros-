fbe9d93bec0963aadcefef88547361d5a2cf159c
Aydom <81184313+aydom669@users.noreply.github.com>
2022-03-15 19:24:30 -0300

    Gerador de numeros

--- script/main.js
+++ script/main.js
@@ -3,19 +3,17 @@
 
 
 const btnCon = document.querySelector(".btn__sortea").addEventListener("click", function(){
-    document.querySelector(".cont__painel").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna2").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna3").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna4").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna5").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna6").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna7").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna8").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna9").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna10").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna11").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna12").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna13").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna14").innerHTML = Math.floor(Math.random() * 25) + 1;
-    document.querySelector(".coluna15").innerHTML = Math.floor(Math.random() * 25) + 1;   
+    function sorteado(max,quant) {
+        var numeroS = []
+        while (numeroS.length < quant) {
+          e = Math.floor(Math.random() * max);
+          if (numeroS.indexOf(e) == -1) {
+            numeroS.push(e)
+          }
+        }
+       return numeroS
+    }
+  document.querySelector(".cont__painel").value = (sorteado(25 , 15))
+   
 });
+
